ceab61b46af9c35c7cdb4dbf99684163ed8ca230
Shaoqun Liu <liuinstein@163.com>
2018-04-21 16:45:12 +0800

    ADD: pop up notification

--- web/assets/js/framework.js
+++ web/assets/js/framework.js
@@ -12,3 +12,13 @@ changeCategory = function (target) {
     $("#activeBarName").text($("#" + target).addClass("active").text().trim());
     localStorage.setItem("activeBar", target);
 };
+
+popNotify = function (level, message) {
+    $.notify({
+        icon: level === 0 ? 'ti-check' : 'ti-close',
+        message: message
+    }, {
+        type: level === 0 ? 'success' : 'danger',
+        timer: 40000
+    });
+};
